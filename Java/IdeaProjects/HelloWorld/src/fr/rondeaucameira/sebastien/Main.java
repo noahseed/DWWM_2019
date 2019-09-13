da39e37a35bc8ef494d64be929e3bdf4e99c0f29
Sébastien RONDEAU CAMEIRA <2760742+noahseed@users.noreply.github.com>
2019-09-13 22:52:26 +0200

    Initial commit

--- Java/IdeaProjects/HelloWorld/src/fr/rondeaucameira/sebastien/Main.java
+++ Java/IdeaProjects/HelloWorld/src/fr/rondeaucameira/sebastien/Main.java
@@ -3,6 +3,6 @@ package fr.rondeaucameira.sebastien;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World");
     }
 }
